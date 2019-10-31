c78647cb5cbabda5ce0326048fd5a0d1140b66ce
nealeellyson <47540354+nealeellyson@users.noreply.github.com>
2019-10-31 08:53:11 -0600

    Tricky-sneaky script

--- LotIDCompare.py
+++ LotIDCompare.py
@@ -12,19 +12,18 @@ import dask.dataframe as dd
 mfile = []
 metro = {}
 metro_lotID = ['lotid']
-for filename in glob.glob("metro/22*"):
+for filename in glob.glob("metro/22*"): # GH v 22 for STII
     metro[filename[6:-4]] = pd.read_csv(filename,usecols=metro_lotID)[metro_lotID] # Using filenames w/out path and file type as dictionary keys
     mfile.append(filename[6:-4])  # Create index of filename strings
     print(f'{filename} data finsished')
     met = pd.concat(metro.values(),ignore_index=True)
-UniqueMetro = met.lotid
+MetroLots = met.lotid
 # Retrieving OES Data from the .csv files, takes MUCH longer, retrieving specfic columns helps
 
 ofile = []
 oes = {}
-oesLots = {}
 data = {}
-lots = {}
+#cols = ['LOT_ID','WAFER_ID','RunStartTime', 'TimeStamp','StepID', 'OES_BCl272', 'OES_C516', 'OES_CN387'] # For STI INTEGRATED
 cols = ['LOT_ID','WAFER_ID','RunStartTime', 'TimeStamp','StepID', 'OES_BCl272', 'OES_C516', 'OES_CN387', 'OES_CO483', 'OES_F703', 'OES_H656', 'OES_O777', 'OES_OH309'] # For STI INTEGRATED
 #cols = ['LOT_ID','WAFER_ID','RunStartTime', 'TimeStamp','StepID', 'OESIB1', 'OESIB2', 'OESIB3', 'OESIB4', 'OESIB5', 'OESIB6', 'OESIB7', 'OESIB8'] # For GH CONTACT 
 for filename in glob.glob("oes/5030*.csv"): #5030-22 STI INTEGRATED DRY ETCH_1 #3500-GH CONTACT OXIDE DRY ETCH_10.csv
@@ -34,24 +33,23 @@ for filename in glob.glob("oes/5030*.csv"): #5030-22 STI INTEGRATED DRY ETCH_1 #
     #data = oes[filename[4:-4]]
     #data.append(files)
     print(f'{filename} data finsished')
-UniqueOES = data.LOT_ID.unique()
-
-new = np.isin(UniqueOES,list(UniqueMetro),invert=True)
+UniqueLots = data.LOT_ID.unique()
+new = np.isin(UniqueLots,list(MetroLots),invert=True)
 i = np.where(new)[0]
-print((UniqueOES),new,i)
-lotID = (UniqueOES[i])
-print(data.size)
+lotID = []
+lotID = (UniqueLots[i])
+# Trim down 'data' to only metro-matching lots by removing 
+indexNames = []
+print('Type:',type(lotID))
+print('Length of lotID:',len(lotID))
+print('Length of data:',len(data))
+for i,x in enumerate(lotID):
+    something = data['LOT_ID']
+    print(len(something))
+    another = data[something == x]
+    indexNames = another.index
+    print('IndexNames:',indexNames)
+    data.drop(indexNames , inplace=True)
+    print('Length:',len(data))
 
 
-
-#print(type(data),len(lotID),len(oesLots))
-#for i,x in enumerate(lotID):
-#    print(i,x,data)
-#    indexNames = data[ data['LOT_ID'] == x ].index
-#    data.drop(indexNames , inplace=True)
-#    lots[i] = (data.loc[data['LOT_ID'] == x])
-    
-#    oesLots = pd.concat(lots.values(),ignore_index=True)
-#print(len(lots),len(lotID),len(oesLots))
-#oesLots.append(oesLots, ignore_index=True)
-#print((oesLots))
